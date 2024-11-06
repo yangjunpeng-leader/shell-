fe422c4f1dfc84cab1147337f6cc247a9df5c13b
yangjp <99852590+yangjunpeng-leader@users.noreply.github.com>
2024-11-06 11:13:37 +0800

    Update jdk17.sh

--- jdk17.sh
+++ jdk17.sh
@@ -6,14 +6,23 @@ if [ "$EUID" -ne 0 ]; then
    exit 1
 fi
 
-# 下载 JDK
-wget -P /opt/ https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+# 检查 /opt/local/ 文件夹是否存在，不存在则创建
+if [! -d "/opt/local/" ]; then
+    mkdir -p /opt/local/
+    echo "创建 /opt/local/ 文件夹成功。"
+fi
+
+# 下载 JDK 到 /opt/local/
+wget -P /opt/local/ https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
 
 # 解压 JDK
-tar -xf /opt/jdk-17_linux-x64_bin.tar.gz -C /opt/
+tar -xf /opt/local/jdk-17_linux-x64_bin.tar.gz -C /opt/local/
+
+# 重命名解压后的文件夹为 jdk
+mv /opt/local/jdk-17.0.8 /opt/local/jdk
 
 # 设置环境变量
-echo "export JAVA_HOME=/opt/jdk-17.0.8" >> /etc/profile
+echo "export JAVA_HOME=/opt/local/jdk" >> /etc/profile
 echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> /etc/profile
 
 # 使环境变量生效
